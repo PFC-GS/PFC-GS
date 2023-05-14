3e75f49c5f66fe5352c3a25f3912120ef50e12e9
Gabriel <gabrielcarrerasmiguel@gmail.com>
2023-05-14 14:27:40 +0200

    Guardado de preguntas en front (falta pulir)

--- tfg-front/src/main/java/org/proyecto/tfgfront/controller/TestController.java
+++ tfg-front/src/main/java/org/proyecto/tfgfront/controller/TestController.java
@@ -1,12 +1,22 @@
 package org.proyecto.tfgfront.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import org.proyecto.tfgfront.model.Pregunta;
 
-public class TestController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class TestController implements Initializable {
 
 
     @FXML
@@ -40,9 +50,18 @@ public class TestController {
 
     @FXML
     private Pane panelTest;
+
+    @FXML
+    private Button siguientePregunta;
+
     private Pane ultimoPanelClickeado = null;
 
-
+    private UniRestController uniRest = new UniRestController();
+    private List<Pregunta> preguntas = uniRest.getPreguntas();
+    private Pregunta preguntaActual;
+    private String respuestaUser;
+    private List<Pregunta> respuesta = new ArrayList<>();
+    private int indicePreguntaActual = 0;
 
 
     @FXML
@@ -62,6 +81,7 @@ public class TestController {
 
     /**
      * Método que actualiza el panel clickeado
+     *
      * @param panelRespuesta panel que se ha clickeado
      */
     private void actualizarPanelClickeado(Pane panelRespuesta) {
@@ -73,11 +93,81 @@ public class TestController {
         ultimoPanelClickeado = panelRespuesta;
     }
 
+    /**
+     * Método que asigna los eventos a los paneles de respuesta
+     * @param panelRespuesta
+     * @param respuesta
+     */
+    private void setPanelRespuestaEvents(Pane panelRespuesta, String respuesta) {
+        panelRespuesta.setOnMouseClicked(event -> {
+            actualizarPanelClickeado(panelRespuesta);
+            respuestaUser = respuesta;
+            preguntaActual.setSolucion(respuestaUser);
+        });
+    }
+
 
     @FXML
     void exit(MouseEvent event) {
         System.exit(0);
     } // TODO: 12/05/2023 implementar un método para salir de la aplicación
 
+    @FXML
+    void siguientePregunta(ActionEvent event) {
 
+        // Guardar la respuesta del usuario para la pregunta actual
+        preguntaActual.setSolucion(respuestaUser);
+        respuesta.add(preguntaActual);
+        respuesta.forEach(System.out::println);
+        System.out.println("-----------------------------------");
+
+        // Incrementar el índice de la pregunta actual
+        indicePreguntaActual++;
+
+        // Comprobar si se han contestado todas las preguntas
+        if (indicePreguntaActual >= preguntas.size()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Test finalizado");
+            alert.setHeaderText("Has finalizado el test");
+            alert.setContentText("Pulsa aceptar para ver los resultados");
+            return;
+        }
+
+        // Establecer la pregunta actual como la siguiente en la lista
+        preguntaActual = preguntas.get(indicePreguntaActual);
+
+        // Actualizar la vista con el contenido de la pregunta actual
+        encabezadoPregunta.setText(preguntaActual.getEnunciado());
+        lbRespuesta1.setText(preguntaActual.getRespuestaA());
+        lbRespuesta2.setText(preguntaActual.getRespuestaB());
+        lbRespuesta3.setText(preguntaActual.getRespuestaC());
+        numeroPregunta.setText(String.valueOf(indicePreguntaActual + 1));
+
+        // Establecer los eventos de clic para cada panel de respuesta
+        setPanelRespuestaEvents(panelRespuesta1, preguntaActual.getRespuestaA());
+        setPanelRespuestaEvents(panelRespuesta2, preguntaActual.getRespuestaB());
+        setPanelRespuestaEvents(panelRespuesta3, preguntaActual.getRespuestaC());
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // mostramos la primera pregunta al usuario y delvolvemos la respuesta segun el panel que se ha clickeado
+        preguntas = uniRest.getPreguntas();
+        if (!preguntas.isEmpty()) {
+            preguntaActual = preguntas.get(0);
+            encabezadoPregunta.setText(preguntaActual.getEnunciado());
+            lbRespuesta1.setText(preguntaActual.getRespuestaA());
+            lbRespuesta2.setText(preguntaActual.getRespuestaB());
+            lbRespuesta3.setText(preguntaActual.getRespuestaC());
+            numeroPregunta.setText("1");
+
+            setPanelRespuestaEvents(panelRespuesta1, preguntaActual.getRespuestaA());
+            setPanelRespuestaEvents(panelRespuesta2, preguntaActual.getRespuestaB());
+            setPanelRespuestaEvents(panelRespuesta3, preguntaActual.getRespuestaC());
+
+        }
+    }
 }
+
