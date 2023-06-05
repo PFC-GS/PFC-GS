77ffdd190adc23a20efd51d27a1e0535b613edee
Gabriel <gabrielcarrerasmiguel@gmail.com>
2023-06-05 14:17:34 +0200

    Agregados javadoc en la parte Servidor

--- proyecto-gabriel-sergio-2k/tfg-rest/src/main/java/org/proyecto/controllers/CategoriaController.java
+++ proyecto-gabriel-sergio-2k/tfg-rest/src/main/java/org/proyecto/controllers/CategoriaController.java
@@ -12,11 +12,19 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Controlador para las categorias
+ */
 @RestController
 public class CategoriaController {
     @Autowired
     private GeneratorService service;
 
+    /**
+     * Devuelve todas las categorias
+     *
+     * @return Lista de categorias
+     */
     @GetMapping(path = "/categorias")
     public ResponseEntity<List<CategoriaDto>> listAllCategorias() {
         return ResponseEntity.ok(service.getAllCategorias()
@@ -24,6 +32,13 @@ public class CategoriaController {
                 .map(CategoriaDto::toDto)
                 .collect(Collectors.toList()));
     }
+
+    /**
+     * Devuelve una categoria por su id
+     *
+     * @param categoriaId Id de la categoria
+     * @return Categoria
+     */
     @GetMapping(path = "/categorias/{categoriaId}")
     public ResponseEntity<CategoriaDto> getCategoriaById(
             @PathVariable("categoriaId") Integer categoriaId) {
@@ -34,6 +49,13 @@ public class CategoriaController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    /**
+     * Crea una categoria
+     *
+     * @param categoria Categoria a crear
+     * @return Categoria creada
+     */
     @PostMapping(path = "/categorias")
     public ResponseEntity<Void> createCategoria(
             @Valid @RequestBody CategoriaDto categoria
@@ -44,6 +66,13 @@ public class CategoriaController {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
+
+    /**
+     * Actualiza una categoria
+     *
+     * @param categoria Categoria a actualizar
+     * @return Categoria actualizada
+     */
     @PutMapping(path = "/categorias/{categoriaId}")
     public ResponseEntity<Void> updateCategoria(
             @Valid @RequestBody CategoriaDto categoria
@@ -54,6 +83,13 @@ public class CategoriaController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    /**
+     * Borra una categoria
+     *
+     * @param categoriaId Id de la categoria a borrar
+     * @return Categoria borrada
+     */
     @DeleteMapping(path = "/categorias/{categoriaId}")
     public ResponseEntity<Void> deleteCategoria(
             @PathVariable("categoriaId") Integer categoriaId
--- proyecto-gabriel-sergio-2k/tfg-rest/src/main/java/org/proyecto/dao/TestDao.java
+++ proyecto-gabriel-sergio-2k/tfg-rest/src/main/java/org/proyecto/dao/TestDao.java
@@ -1,6 +1,5 @@
 package org.proyecto.dao;
 
-import org.proyecto.Entity.Pregunta;
 import org.proyecto.Entity.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,14 +8,29 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Set;
 
+/**
+ * Interfaz DAO de Test .
+ */
 @Repository
-public interface TestDao extends JpaRepository<Test,Integer> {
+public interface TestDao extends JpaRepository<Test, Integer> {
+
+    /**
+     * Método para obtener el último test realizado por un usuario
+     *
+     * @param usuarioId id del usuario
+     * @return Test
+     */
     @Query(value = "SELECT * FROM test WHERE fecha = :fecha AND usuario = :usuarioId ORDER BY id DESC LIMIT 1", nativeQuery = true)
-    Test findTestByUserIdAndDate(@Param("usuarioId")Integer usuarioId, @Param("fecha") Timestamp fecha);
-//select * from test where usuario = 1 order by fecha desc
+    Test findTestByUserIdAndDate(@Param("usuarioId") Integer usuarioId, @Param("fecha") Timestamp fecha);
+
+    /**
+     * Método para obtener todos los test realizados por un usuario
+     *
+     * @param usuarioId id del usuario
+     * @return List de Test
+     */
     @Query(value = "SELECT * FROM test WHERE usuario = :usuarioId ORDER BY fecha desc", nativeQuery = true)
-    List<Test> findTestByUserId(@Param("usuarioId")Integer usuarioId);
+    List<Test> findTestByUserId(@Param("usuarioId") Integer usuarioId);
 }
 
